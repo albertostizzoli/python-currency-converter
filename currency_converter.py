d26e1b7ab0da5e3aef3de0ef378c103cc031c71b
albertostizzoli <albertostizzoli@yahoo.it>
2025-07-24 16:42:17 +0200

    create function for to get exchange rates

---
+++ currency_converter.py
@@ -0,0 +1,15 @@
+# api_key = 1ccaeda55dbb420baa90350d
+
+import requests
+
+# creo una funzione per ottenere i tassi di cambio dall'API
+def get_exchange_rates(api_key, base_currency, target_currency):
+    url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{base_currency}"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        rates = data['conversion_rates']
+        return rates[target_currency]
+    else:
+        raise Exception(f"Errore nell'ottenere i tassi di cambio: {data['error-type']}")
+    
