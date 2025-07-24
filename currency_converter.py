27bd07f6a0cdefd80a232c2850f812cd5f5f4b89
albertostizzoli <albertostizzoli@yahoo.it>
2025-07-24 16:47:07 +0200

    create function for  to convert currency amount

--- currency_converter.py
+++ currency_converter.py
@@ -1,4 +1,3 @@
-# api_key = 1ccaeda55dbb420baa90350d
 
 import requests
 
@@ -12,4 +11,14 @@ def get_exchange_rates(api_key, base_currency, target_currency):
         return rates[target_currency]
     else:
         raise Exception(f"Errore nell'ottenere i tassi di cambio: {data['error-type']}")
+    
+# creo una funzione per convertire l'importo delle valute
+def convert_currency(amount, base_currency, target_currency, api_key):
+    rate = get_exchange_rates(api_key, base_currency, target_currency)
+    converted_amount = amount * rate
+    return converted_amount
+
+def main():
+    api_key = "1ccaeda55dbb420baa90350d"
+    print("CONVERTITORE DI VALUTE")
     
